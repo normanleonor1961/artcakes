7e3327b8f0dad98fbd45945d42c1d14a33d0943f
normanleonor1961 <109060058+normanleonor1961@users.noreply.github.com>
2022-09-07 10:58:27 +0800

    okay

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -11,7 +11,10 @@ const App = () => {
       <Header/>
       <main className="py-3" >
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/ '
+          </Routes>
         </Container>
       </main>
       <Footer/>
