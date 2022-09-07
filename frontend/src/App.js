d5bee881c71aa12edce928dde92fea20da742190
normanleonor1961 <109060058+normanleonor1961@users.noreply.github.com>
2022-09-07 14:31:46 +0800

    With react-router-dom Issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Container } from 'react-bootstrap'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
@@ -11,10 +12,10 @@ const App = () => {
       <Header/>
       <main className="py-3" >
         <Container>
-          <Routes>
-            <Route path='/' element={<HomeScreen />} />
-            <Route path='/ '
-          </Routes>
+            <Routes>
+              <Route path='/' element={<HomeScreen/>}  />
+              <Route path='/product/:id' element={<ProductScreen/>} />
+            </Routes>
         </Container>
       </main>
       <Footer/>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
         <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
         <Container>
-            <Navbar.Brand href="/">Art Cakes</Navbar.Brand>
+            <Navbar.Brand href='/'>Art Cakes</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
